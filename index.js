7156b8c28e065a154ab03373a4a1ea7033760855
Will B Chang <will.b.chang@gmail.com>
2023-08-14 15:56:30 +0800

    pdf: extract aws link from pdf page

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ const options = {};
 pdfExtract.extractBuffer(buffer, options, (err, data) => {
   if (err) return console.log(err);
   console.log(data.pages)
+
+  const link = data.pages
+    .map(page => page.links.find(link => link.includes('s3.amazonaws.com')))
+    .find(link => link)
+  console.log(link)
 });
