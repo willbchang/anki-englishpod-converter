0489022967c480e3538df24e3624aa2e2e066659
Will B Chang <will.b.chang@gmail.com>
2023-08-15 13:51:07 +0800

    refactor: extract deck.js

---
+++ deck.js
@@ -0,0 +1,26 @@
+import { JSDOM } from 'jsdom'
+
+export function getDeck (html) {
+  const { document } = (new JSDOM(html)).window
+  return  getDeckName(document) + '\n' + getCardContent(document)
+
+  function getDeckName() {
+    const name = document.querySelector('h1:first-child a')
+      .textContent
+      .replace(/^.*- /, '')
+      .replace('!', '')
+    const index = document.querySelector('h1:first-child span')
+      .textContent
+      .replace(/[()]/g, '')
+      .replace(/^0/, '')
+    return '# EnglishPod 365::' + index + 'B ' + name
+  }
+
+  function getCardContent() {
+    const tds = [...document.querySelectorAll('h1:not(:first-child) + table tr td:nth-child(odd)')]
+    return tds.map((td, index) => {
+      const prefix = index % 2 === 0 ? '## ' : ''
+      return prefix + td.textContent
+    }).join('\n')
+  }
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {PDFExtract} from 'pdf.js-extract';
 import fs from 'fs'
-import { JSDOM } from 'jsdom'
+import { getDeck } from './deck.js'
 import { createDeckWithNotes } from './anki.js'
 
 extractPDF('test.pdf')
@@ -23,27 +23,4 @@ function getLink (data) {
     .find(link => link)
 }
 
-function getDeck (html) {
-  const { document } = (new JSDOM(html)).window
-  return  getDeckName(document) + '\n' + getCardContent(document)
 
-  function getDeckName() {
-    const name = document.querySelector('h1:first-child a')
-      .textContent
-      .replace(/^.*- /, '')
-      .replace('!', '')
-    const index = document.querySelector('h1:first-child span')
-      .textContent
-      .replace(/[()]/g, '')
-      .replace(/^0/, '')
-    return '# EnglishPod 365::' + index + 'B ' + name
-  }
-
-  function getCardContent() {
-    const tds = [...document.querySelectorAll('h1:not(:first-child) + table tr td:nth-child(odd)')]
-    return tds.map((td, index) => {
-      const prefix = index % 2 === 0 ? '## ' : ''
-      return prefix + td.textContent
-    }).join('\n')
-  }
-}
