5d18fa221ae8131fd718b3a554da70fb888ba765
Will B Chang <will.b.chang@gmail.com>
2023-08-15 13:08:05 +0800

    feat: invoke anki connect to createDeck

--- anki.js
+++ anki.js
@@ -1,16 +1,22 @@
+createDeck()
+export async function createDeck (params) {
+  await invoke('createDeck', 6, { deck: 'test1' })
+  const result = await invoke('deckNames', 6)
+  console.log(result)
+}
+
 function invoke (action, version, params = {}) {
   const url = 'http://127.0.0.1:8765'
-  return fetch(url, {
+  const config = {
     method: 'POST',
     body: JSON.stringify({
       action,
       version,
       params,
     })
-  })
+  }
+  return fetch(url, config)
+    .then(response => response.json())
 }
 
-await invoke('createDeck', 6, { deck: 'test1' })
-const result = await invoke('deckNames', 6)
-console.log(`got list of decks: ${result}`)
 
