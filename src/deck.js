96bb03789a5b2fcf17b20e47067d20d432630e5d
Will B Chang <will.b.chang@gmail.com>
2023-08-15 14:00:39 +0800

    refactor: move functions out

--- src/deck.js
+++ src/deck.js
@@ -3,24 +3,24 @@ import { JSDOM } from 'jsdom'
 export function generateDeck (html) {
   const { document } = (new JSDOM(html)).window
   return  getDeckName(document) + '\n' + getCardContent(document)
+}
 
-  function getDeckName() {
-    const name = document.querySelector('h1:first-child a')
-      .textContent
-      .replace(/^.*- /, '')
-      .replace('!', '')
-    const index = document.querySelector('h1:first-child span')
-      .textContent
-      .replace(/[()]/g, '')
-      .replace(/^0/, '')
-    return '# EnglishPod 365::' + index + 'B ' + name
-  }
+function getDeckName() {
+  const name = document.querySelector('h1:first-child a')
+    .textContent
+    .replace(/^.*- /, '')
+    .replace('!', '')
+  const index = document.querySelector('h1:first-child span')
+    .textContent
+    .replace(/[()]/g, '')
+    .replace(/^0/, '')
+  return '# EnglishPod 365::' + index + 'B ' + name
+}
 
-  function getCardContent() {
-    const tds = [...document.querySelectorAll('h1:not(:first-child) + table tr td:nth-child(odd)')]
-    return tds.map((td, index) => {
-      const prefix = index % 2 === 0 ? '## ' : ''
-      return prefix + td.textContent
-    }).join('\n')
-  }
+function getCardContent() {
+  const tds = [...document.querySelectorAll('h1:not(:first-child) + table tr td:nth-child(odd)')]
+  return tds.map((td, index) => {
+    const prefix = index % 2 === 0 ? '## ' : ''
+    return prefix + td.textContent
+  }).join('\n')
 }
