43273b720dc06a18da1019085469b8fe96c106b2
Will B Chang <will.b.chang@gmail.com>
2023-08-15 14:27:34 +0800

    feat: extract deck name index from url

--- src/deck.js
+++ src/deck.js
@@ -1,23 +1,24 @@
 import { JSDOM } from 'jsdom'
 
-export function generateDeck (html) {
+export function generateDeck (html, url) {
   const { document } = (new JSDOM(html)).window
-  return  getDeckName(document) + '\n' + getCardContent(document)
+  return  getDeckName(document, url) + '\n' + getCardContent(document)
 }
 
-function getDeckName() {
+function getDeckName(document, url) {
+  const index = url.split('englishpod_')[1]
+    .replace('.html', '')
+    .replace('0', '')
+
   const name = document.querySelector('h1:first-child a')
     .textContent
     .replace(/^.*- /, '')
     .replace('!', '')
-  const index = document.querySelector('h1:first-child span')
-    .textContent
-    .replace(/[()]/g, '')
-    .replace(/^0/, '')
-  return '# EnglishPod 365::' + index + 'B ' + name
+
+  return 'EnglishPod 365::' + index + ' ' + name
 }
 
-function getCardContent() {
+function getCardContent(document) {
   const tds = [...document.querySelectorAll('h1:not(:first-child) + table tr td:nth-child(odd)')]
   return tds.map((td, index) => {
     const prefix = index % 2 === 0 ? '## ' : ''
