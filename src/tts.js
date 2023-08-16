2b14be6257015cac8b15921e32fa8e9492717435
Will B Chang <will.b.chang@gmail.com>
2023-08-16 10:28:14 +0800

    refactor: extract fetchTTS()

--- src/tts.js
+++ src/tts.js
@@ -1,28 +1,28 @@
 import fs from 'fs'
 
-const url = `https://${process.env.AZURE_REGION}.tts.speech.microsoft.com/cognitiveservices/v1`
-
-const body = `
+function fetchTTS (text) {
+  const url = `https://${process.env.AZURE_REGION}.tts.speech.microsoft.com/cognitiveservices/v1`
+  const config = {
+    method: 'POST',
+    headers: {
+      'Ocp-Apim-Subscription-Key': process.env.AZURE_SUBSCRIPTION_KEY,
+      'Content-Type': 'application/ssml+xml',
+      'X-Microsoft-OutputFormat': 'audio-16khz-32kbitrate-mono-mp3',
+      'User-Agent': 'Anki',
+    },
+    body: `
 <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en-US">
     <voice name="en-US-JennyMultilingualV2Neural">
-        Be reverent before the dawning day. Do not think of what will be in a year, or in ten years. Think of to-day.
+        ${text}
     </voice>
 </speak>
 `
+  }
 
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Ocp-Apim-Subscription-Key': process.env.AZURE_SUBSCRIPTION_KEY,
-    'Content-Type': 'application/ssml+xml',
-    'X-Microsoft-OutputFormat': 'audio-16khz-32kbitrate-mono-mp3',
-    'User-Agent': 'Anki',
-  },
-  body,
-})
-  .then(response => response.arrayBuffer())
-  .then(saveAsMp3)
-  .catch(error => console.error('Error:', error))
+  return fetch(url, config)
+    .then(response => response.arrayBuffer())
+    .catch(error => console.error('Error:', error))
+}
 
 function saveAsMp3 (arrayBuffer) {
   const buffer = Buffer.from(arrayBuffer)
