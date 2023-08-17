502be60ecf50a087843fa256554cb01a82ca3db9
Will B Chang <will.b.chang@gmail.com>
2023-08-17 13:45:01 +0800

    fix: guessing link with recursion

--- index.js
+++ index.js
@@ -27,11 +27,23 @@ function convertEnglishPodToAnki (filepath) {
     .catch(error => console.error(error))
 }
 
-async function fetchResponse (link) {
+async function fetchResponse (link, count = -1) {
   const response = await fetch(link)
-  const html = await response.text()
-  const url = response.url
-  return { html, url }
+  const levels = ['B', 'C', 'D', 'E']
+  let html, url = ''
+  if (!response.ok) {
+    count += 1
+    if (count >= levels.length) throw new Error('No Valid Link')
+    const newLink = link.replace(/[A-Z]?(\d+\.html)/, levels[count] + '$1')
+    console.log('Link is not Valid: ', response.url)
+    console.log('Trying: ', newLink)
+    return await fetchResponse(newLink, count)
+  } else {
+    html = await response.text()
+    url = response.url
+    console.log('Found Valid Link: ', url)
+    return { html, url }
+  }
 }
 
 async function removeAudioFiles () {
